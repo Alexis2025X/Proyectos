d6c92214b2fdcc66f8f1a51e818aff9c7bfea9c7
Marlon <marlo@MARLON>
2024-09-19 09:05:37 -0600

    algo prueba

--- Proyecto_Personal1/Proveedores.Designer.cs
+++ Proyecto_Personal1/Proveedores.Designer.cs
@@ -58,7 +58,7 @@ namespace Proyecto_Personal1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(343, 95);
+            this.button1.Location = new System.Drawing.Point(192, 274);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(385, 197);
             this.button1.TabIndex = 4;
